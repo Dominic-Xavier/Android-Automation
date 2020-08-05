064f52b6704b4ffe76f0b753bb82c0a22dce57f0
Home <Home@DESKTOP-9D0UF9G>
2020-08-05 13:08:20 +0530

    Financial Manager using testng

---
+++ com.FinancialMaganer/src/main/java/com/retry/FailedTestCase/RetryAnalyzer.java
@@ -0,0 +1,21 @@
+package com.retry.FailedTestCase;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+	
+	int count = 0;
+	int retrylimit = 3;
+	
+	@Override
+	public boolean retry(ITestResult result) {
+		
+		if(count<retrylimit) {
+			count++;
+			return true;
+		}
+		return false;
+	}
+
+}
